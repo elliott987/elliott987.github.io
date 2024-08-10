9b6fbda1652f46985c4c3144b539355f148c1ed4
elliott987 <96375838+elliott987@users.noreply.github.com>
2024-08-10 08:40:17 +0100

    mpld3 test

--- test.py
+++ test.py
@@ -1,43 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+import mpld3
 
-# Constants
-g = 10
-u = 10.0
-
-def calculate_r(t, theta, u, g):
+def calculate_trajectory(theta, g, u, h, delta_t):
     theta_rad = np.radians(theta)
-    r = np.sqrt((u**2 * t**2) - (g* t**3 * u * np.sin(theta_rad)) + (1/4 * (g**2) * (t**4)))
-    return r
-
-def calculate_xy(t, theta, u, g):
-    theta_rad = np.radians(theta)
-    x = u * t * np.cos(theta_rad)
-    y = u * t * np.sin(theta_rad) - 0.5 * g * t**2
+    t_max = (u * np.sin(theta_rad) + np.sqrt((u * np.sin(theta_rad))**2 + 2 * g * h)) / g
+    t = np.arange(0, t_max + delta_t, delta_t)
+    x = u * np.cos(theta_rad) * t
+    y = h + u * np.sin(theta_rad) * t - 0.5 * g * t**2
     return x, y
 
-thetas = [30, 45, 60, 70.5, 78, 85]
-time_values = np.arange(0.1, 5.1, 0.1)
+theta_init = 45
+g_init = 9.81
+u_init = 20.0
+h_init = 0.0
+delta_t_init = 0.01
 
-for max_time in time_values:
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
-    t = np.linspace(0, max_time, 1000)
-    
-    for theta in thetas:
-        r = calculate_r(t, theta, u, g)
-        x, y = calculate_xy(t, theta, u, g)
-        ax1.plot(t, r, label=f'θ = {theta}°')
-        ax2.plot(x, y, label=f'θ = {theta}°')
-    
-    ax1.set_xlabel('Time (s)')
-    ax1.set_ylabel('Range (m)')
-    ax1.set_title('Range vs Time')
-    ax1.legend()
+fig, ax = plt.subplots()
+plt.subplots_adjust(left=0.1, bottom=0.3)
+x, y = calculate_trajectory(theta_init, g_init, u_init, h_init, delta_t_init)
+line, = plt.plot(x, y, lw=2)
+ax.set_xlabel('Distance (m)')
+ax.set_ylabel('Height (m)')
+ax.set_title('Task 1')
 
-    ax2.set_xlabel('Distance (m)')
-    ax2.set_ylabel('Height (m)')
-    ax2.set_title('Height vs Distance')
-    ax2.legend()
+ax.set_xlim(0, 100)
+ax.set_ylim(0, 50)
 
-    plt.savefig(f'plots/plot_{int(max_time*10)}.png')
-    plt.close(fig)
+axcolor = 'lightgoldenrodyellow'
+ax_theta = plt.axes([0.1, 0.2, 0.65, 0.03], facecolor=axcolor)
+ax_u = plt.axes([0.1, 0.15, 0.65, 0.03], facecolor=axcolor)
+ax_h = plt.axes([0.1, 0.1, 0.65, 0.03], facecolor=axcolor)
+ax_dt = plt.axes([0.1, 0.05, 0.65, 0.03], facecolor=axcolor)
+
+s_theta = Slider(ax_theta, 'Angle (deg)', 0, 90, valinit=theta_init)
+s_u = Slider(ax_u, 'Speed (m/s)', 0, 100, valinit=u_init)
+s_h = Slider(ax_h, 'Height (m)', 0, 100, valinit=h_init)
+s_dt = Slider(ax_dt, 'Delta t (s)', 0.001, 1, valinit=delta_t_init)
+
+def update(val):
+    theta = s_theta.val
+    u = s_u.val
+    h = s_h.val
+    delta_t = s_dt.val
+    x, y = calculate_trajectory(theta, g_init, u, h, delta_t)
+    line.set_xdata(x)
+    line.set_ydata(y)
+    fig.canvas.draw_idle()
+
+s_theta.on_changed(update)
+s_u.on_changed(update)
+s_h.on_changed(update)
+s_dt.on_changed(update)
+
+plt.show()
+html_str = mpld3.fig_to_html(fig)
+Html_file= open("index.html","w")
+Html_file.write(html_str)
+Html_file.close()
